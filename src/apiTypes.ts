d0e7932261d276c7c209b2eb0ab318cd4647e3cc
Richard Huss <richard@huss.org.uk>
2026-09-18 20:38:37 +0100

    WIP: bit more documentation of presentation types

--- src/apiTypes.ts
+++ src/apiTypes.ts
@@ -3,30 +3,30 @@ export interface ProclaimAuthResponse {
 	proclaimAuthToken: string
 }
 
-enum aspectRatio {
+export enum aspectRatio {
 	SixteenByNine = 'SixteenByNine',
 	FourByThree = 'FourByThree',
 }
 
 // Response from /presentations/onair endpoint
 export interface Presentation {
-	sessionId: string
-	groupId: string
-	groupName: string
-	id: string
-	localRevision: number
-	title: string
+	sessionId: string // GUID, distinct from the on-air session ID returned from /onair/session
+	groupId: string // The group that the presentation belongs to
+	groupName: string // Name of the group that the presentation belongs to
+	id: string // GUID
+	localRevision: number // Timestamp of the last time the presentation was modified
+	title: string // Presentation title, e.g. "Sunday Service"
 	logoServiceItemId: string
-	dateGiven: number
+	dateGiven: number // The service date, but not time of day
 	startTime: number // When the presentation went on-air, *not* the scheduled start time!
-	aspectRatio: aspectRatio
-	warmupStartIndex: number
-	serviceStartIndex: number
-	postServiceStartIndex: number
-	serviceItems: Array<ServiceItem>
+	aspectRatio: aspectRatio // The aspect ratio of the presentation
+	warmupStartIndex: number // Index of the first service item in the warmup service section
+	serviceStartIndex: number // Index of the first service item in the service section
+	postServiceStartIndex: number // Index of the first service item in the post-service section
+	serviceItems: Array<ServiceItem> // The service items within the presentation
 }
 
-enum ServiceItemKind {
+export enum ServiceItemKind {
 	Announcement = 'Announcement',
 	Content = 'Content',
 	StageDirectionCue = 'StageDirectionCue',
@@ -36,16 +36,16 @@ enum ServiceItemKind {
 }
 
 // Items within a presentation
-interface ServiceItem {
-	id: string
-	title: string
-	notes: string
-	kind: ServiceItemKind
-	slides: Array<Slide>
+export interface ServiceItem {
+	id: string // GUID of the service item
+	title: string // Title of the service item
+	notes: string // Notes for the service item
+	kind: ServiceItemKind // The kind of service item, e.g. "SongLyrics"
+	slides: Array<Slide> // The individual slides within the service item
 }
 
 // Slides within a service item
-interface Slide {
-	localRevision: number
-	index: number
+export interface Slide {
+	localRevision: number // Timestamp of the last time the slide was modified
+	index: number // Slide index within the service item
 }
